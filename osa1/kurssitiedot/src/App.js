da3f4b1d2d3aeba996a7bb5f1d415449e54ae8b1
Tommi Seppä <tommi.seppa@tuni.fi>
2021-07-03 13:30:08 +0300

    1.5 ready

--- osa1/kurssitiedot/src/App.js
+++ osa1/kurssitiedot/src/App.js
@@ -4,7 +4,7 @@ import React from 'react'
 const Header = (props) => {
   return (
     <>
-      <h1>{props.course}</h1>
+      <h1>{props.course['name']}</h1>
     </>
   )
 }
@@ -40,27 +40,29 @@ const Total = (props) => {
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
       <Header course={course} />
-      <Content parts={parts} />
-      <Total exercises1={parts[0].exercises} exercises2={parts[1].exercises} exercises3={parts[2].exercises} />
+      <Content parts={course['parts']} />
+      <Total exercises1={course['parts'][0]['exercises']} exercises2={course['parts'][1]['exercises']} exercises3={course['parts'][2]['exercises']} />
     </div>
   )
 }
